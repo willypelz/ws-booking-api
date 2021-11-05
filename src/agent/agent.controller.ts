a3670d739446a4b839498a52efff2a8a256cd5b4
willypelz <pelumiasefon@gmail.com>
2021-11-05 17:19:50 +0100

    [IMP] update age module

--- src/agent/agent.controller.ts
+++ src/agent/agent.controller.ts
@@ -26,15 +26,15 @@ export class AgentController {
   }
 
   //
-  // @Get(':slug')
-  // async findOne(@Param('slug') slug): Promise<AgentRO> {
-  //   return await this.agentService.findOne({slug});
-  // }
-  //
-  // // @Get(':slug/agents')
-  // // async findAgents(@Param('slug') slug): Promise<AgentsRO> {
-  // //   return await this.agentService.findAgents(slug);
-  // // }
+  @Get(':slug')
+  async findOne(@Param('slug') slug): Promise<AgentRO> {
+    return await this.agentService.findOne({slug});
+  }
+
+  @Get(':slug/agents')
+  async findAgents(@Param('slug') slug): Promise<AgentsRO> {
+    // return await this.agentService.findAgents(slug);
+  }
   //
   @ApiOperation({ summary: 'Create agent' })
   @ApiResponse({ status: 201, description: 'The agent has been successfully created.'})
@@ -44,39 +44,23 @@ export class AgentController {
     return this.agentService.create(userId, agentData);
   }
 
-  // @ApiOperation({ summary: 'Update agent' })
-  // @ApiResponse({ status: 201, description: 'The agent has been successfully updated.'})
-  // @ApiResponse({ status: 403, description: 'Forbidden.' })
-  // @Put(':slug')
-  // async update(@Param() params, @Body('agent') agentData: CreateAgentDto) {
-  //   // Todo: update slug also when title gets changed
-  //   return this.agentService.update(params.slug, agentData);
-  // }
-  //
-  // @ApiOperation({ summary: 'Delete agent' })
-  // @ApiResponse({ status: 201, description: 'The agent has been successfully deleted.'})
-  // @ApiResponse({ status: 403, description: 'Forbidden.' })
-  // @Delete(':slug')
-  // async delete(@Param() params) {
-  //   return this.agentService.delete(params.slug);
-  // }
-  // //
-  // // @ApiOperation({ summary: 'Create agent' })
-  // // @ApiResponse({ status: 201, description: 'The agent has been successfully created.'})
-  // // @ApiResponse({ status: 403, description: 'Forbidden.' })
-  // // @Post(':slug/agents')
-  // // async createAgent(@Param('slug') slug, @Body('agent') agentData: CreateAgentDto) {
-  // //   return await this.agentService.addAgent(slug, agentData);
-  // // }
-  //
-  // @ApiOperation({ summary: 'Delete agent' })
-  // @ApiResponse({ status: 201, description: 'The agent has been successfully deleted.'})
-  // @ApiResponse({ status: 403, description: 'Forbidden.' })
-  // @Delete(':slug/agents/:id')
-  // async deleteAgent(@Param() params) {
-  //   const {slug, id} = params;
-  //   return await this.agentService.deleteAgent(slug, id);
-  // }
+  @ApiOperation({ summary: 'Update agent' })
+  @ApiResponse({ status: 201, description: 'The agent has been successfully updated.'})
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
+  @Put(':slug')
+  async update(@Param() params, @Body('agent') agentData: CreateAgentDto) {
+    // Todo: update slug also when title gets changed
+    return this.agentService.update(params.slug, agentData);
+  }
+
+  @ApiOperation({ summary: 'Delete agent' })
+  @ApiResponse({ status: 201, description: 'The agent has been successfully deleted.'})
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
+  @Delete(':slug/agents/:id')
+  async deleteAgent(@Param() params) {
+    const {slug, id} = params;
+    // return await this.agentService.deleteAgent(slug, id);
+  }
 
 
 }
--- src/agent/agent.entity.ts
+++ src/agent/agent.entity.ts
@@ -21,17 +21,6 @@ export class AgentEntity {
   @Column()
   email: string;
 
-  @Column({ type: 'timestamp', default: () => "CURRENT_TIMESTAMP"})
-  created: Date;
-
-  @Column({ type: 'timestamp', default: () => "CURRENT_TIMESTAMP"})
-  updated: Date;
-
-  @BeforeUpdate()
-  updateTimestamp() {
-    this.updated = new Date;
-  }
-
   @OneToMany(BookingEntity => BookingEntity, booking => booking.id)
   @JoinTable()
   booking: BookingEntity[];
